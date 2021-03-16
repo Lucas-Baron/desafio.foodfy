14b8cd8d18fcee0571fea73afc4d22a275fbeca1
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-16 14:56:09 -0300

    Criado data.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
+    const modalOverlay = document.querySelector('.modal-overlay')
+    const cards = document.querySelectorAll('.card')
 
-const text = document.querySelector('.card_text')
 
-for (let card of cards){
-    card.addEventListener("click", function(){
-        const imageId = card.getAttribute('id')
-        modalOverlay.classList.add('active')
 
-        modalOverlay.querySelector('img').src = `/assets/${imageId}`
+    for (let card of cards){
+        card.addEventListener("click", function(){
+            const textH2 = card.getAttribute('p')
 
+            const imageId = card.getAttribute('id')
+            modalOverlay.classList.add('active')
+
+            modalOverlay.querySelector('img').src = `/assets/${imageId}`
+
+
+            modalOverlay.querySelector('h2').innerHTML = `${textH2}`
+
+        })
+    }
+
+    document.querySelector('.modal-close').addEventListener("click", function(){
+        modalOverlay.classList.remove('active')
     })
-}
 
-document.querySelector('.modal-close').addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
-})
